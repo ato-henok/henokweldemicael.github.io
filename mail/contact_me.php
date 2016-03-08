ccd622b7719a343c25618bec639d7a3fd02188f1
Henok WeldeMicael <weldemicael.1@osu.edu>
2016-03-08 16:20:28 -0600

    Portfolio Icons, Module done. Description still to be added.

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -17,7 +17,6 @@ $message = $_POST['message'];
 	
 // Create the email and send the message
 $to = 'henokweldemicael@gmail.com'; 
-- This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@henokweldemicael.github.io\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
